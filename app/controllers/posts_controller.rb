dba08eb3a8769e030ec5b15752a2ac76cf3bcefe
AndyLanYT <AndyLanYT@gmail.com>
2021-09-02 17:05:04 +0300

    Added posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new 
+  end
+
+  def index
+    posts = Post.all
+
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+
+    render json: post
+  end
+
+  def create
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update
+
+    render json: post
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post
+      post.destroy
+      render json: { status: 'Successfully destroyed!' }
+    else
+      render json: { error: 'Post not found!' }, status: :not_found
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+end
