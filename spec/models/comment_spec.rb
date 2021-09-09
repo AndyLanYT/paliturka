040129460288c016682daea50d08b476868e1eb3
AndyLanYT <AndyLanYT@gmail.com>
2021-09-09 12:56:02 +0300

    Updated models tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,23 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:current_user) do
-    User.create!(
-      id: 153,
-      email: 'email@example.com',
-      password: 'password'
-    )
-  end
-  let(:current_post) do
-    Post.create!(
-      id: 239,
-      body: 'Just a body',
-      user: current_user
-    )
-  end
+  let(:user) { create_user }
+  let(:current_post) { create_post(user: user) }
 
   it 'has a body length maximum 100 charecters' do
-    comment = described_class.new(body: nil, user: current_user, post: current_post)
+    comment = described_class.new(body: nil, user: user, post: current_post)
     expect(comment).to be_valid
 
     comment.body = '*' * 100
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:user) do
-    User.create!(
-      id: 1,
-      email: 'test@example.com',
-      password: 'password'
-    )
-  end
+  let(:user) { create_user }
 
   it 'can not have a body' do
     post = described_class.new(body: nil, user: user)
