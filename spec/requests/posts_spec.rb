91444f750b5344640592e84f335bad9af2238bea
AndyLanYT <AndyLanYT@gmail.com>
2021-09-14 23:09:09 +0300

    Used rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -62,61 +62,62 @@ RSpec.describe 'Post CRUD', type: :request do
   end
 
   # UPDATE
-  context 'PUT /api/v1/posts/:id' do
+  context 'when PUT /api/v1/posts/:id' do
     context 'without a user' do
       it 'returns a 401' do
         record = create_post
         put "/api/v1/posts/#{record.id}", params: '{ "post": { "body": "New body" } }'
-        expect(response).to have_http_status(404)
+        expect(response).to have_http_status(:not_found)
       end
     end
 
-    context 'as a non-user of post' do
-      it 'should not let me update a post' do
+    context 'with a non-user of post' do
+      it 'does not let me update a post' do
         user1 = create_user
         user2 = create_user
         record = create_post({ user: user1 })
-        put "/api/v1/posts/#{record.id}", params: '{ "post": { "body": "New body" } }', headers: get_headers(user2.email, user2.password)
-        expect(response).to have_http_status(404)
+        put "/api/v1/posts/#{record.id}", params: '{ "post": { "body": "New body" } }',
+                                          headers: get_headers(user2.email, user2.password)
+        expect(response).to have_http_status(:not_found)
       end
     end
 
-    context 'as a org owner' do
-      it 'should let me update a post' do
+    context 'with an org owner' do
+      it 'lets me update a post' do
         record = create_post({ user: user })
         put "/api/v1/posts/#{record.id}", params: '{ "post": { "body": "New body" } }', headers: auth_headers
         parsed = JSON.parse(response.body, object_class: OpenStruct)
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
         expect(parsed.body).to eq('New body')
       end
     end
   end
 
   # DESTROY
-  context 'DELETE /api/v1/posts/:id' do
+  context 'when DELETE /api/v1/posts/:id' do
     context 'without a user' do
       it 'returns a 404' do
         record = create_post
         delete "/api/v1/posts/#{record.id}"
-        expect(response).to have_http_status(404)
+        expect(response).to have_http_status(:not_found)
       end
     end
 
-    context 'as a user' do
-      it 'should not let me destroy a post' do
+    context 'with a user' do
+      it 'does not let me destroy a post' do
         user1 = create_user
         user2 = create_user
         record = create_post({ user: user1 })
         delete "/api/v1/posts/#{record.id}", headers: get_headers(user2.email, user2.password)
-        expect(response).to have_http_status(404)
+        expect(response).to have_http_status(:not_found)
       end
     end
 
-    context 'as a user (owner)' do
-      it 'should let me destroy a post' do
+    context 'with a user (owner)' do
+      it 'lets me destroy a post' do
         record = create_post({ user: user })
         delete "/api/v1/posts/#{record.id}", headers: auth_headers
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
   end
