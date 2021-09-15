768a1e386ddf367afbc89f4097087763a0a64546
AndyLanYT <AndyLanYT@gmail.com>
2021-09-15 23:50:29 +0300

    Generated avatar uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_224648) do
+ActiveRecord::Schema.define(version: 2021_09_15_204648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,18 @@ ActiveRecord::Schema.define(version: 2021_09_02_224648) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.bigint "user_id"
+    t.bigint "thread_id"
+    t.bigint "parent_id"
+    t.text "body", null: false
+    t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["parent_id"], name: "index_comments_on_parent_id"
+    t.index ["thread_id"], name: "index_comments_on_thread_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -45,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_224648) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -60,13 +67,12 @@ ActiveRecord::Schema.define(version: 2021_09_02_224648) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "allowlisted_jwts", "users", on_delete: :cascade
-  add_foreign_key "comments", "posts", on_delete: :cascade
-  add_foreign_key "comments", "users", on_delete: :cascade
   add_foreign_key "posts", "users", on_delete: :cascade
 end
