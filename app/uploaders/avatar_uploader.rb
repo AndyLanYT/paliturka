2d5214a913e27f05dd607f42c5b3474e6f53a330
AndyLanYT <AndyLanYT@gmail.com>
2021-09-16 16:58:15 +0300

    Added user and avatar tests

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,7 +22,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale: [100, 100]
+  process resize_to_fit: [400, 400]
   #
   # def scale(width, height)
   #   # do something
@@ -30,7 +30,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fit: [100, 100]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
---
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'carrierwave/test/matchers'
+
+describe AvatarUploader do
+  include CarrierWave::Test::Matchers
+
+  let(:user) { create_user }
+  let(:uploader) { described_class.new(user, :avatar) }
+
+  before do
+    described_class.enable_processing = true
+    File.open(Rails.root.join('spec/fixtures/files/standard_avatar.png')) { |f| uploader.store!(f) }
+  end
+
+  after do
+    described_class.enable_processing = false
+    uploader.remove!
+  end
+
+  it 'makes the image size 100 by 100 pixels' do
+    expect(uploader.thumb).to have_dimensions(100, 100)
+  end
+
+  it 'makes the image to fit within 200 by 200 pixels' do
+    expect(uploader).to be_no_larger_than(400, 400)
+  end
+
+  it 'has .jpg .jpeg or .png format' do
+    expect(uploader).to be_format('png')
+  end
+end
